a8fe02eb9f5338897527d886520b3a51663c1746
Hyojoon Jung <hyojoon119@gmail.com>
2025-04-25 17:11:26 -0700

    This is the code with the bug fix and a simple code that I applied with player's distributions

--- simulations/builder_auctions/main.py
+++ simulations/builder_auctions/main.py
@@ -1,6 +1,7 @@
 import random
 from formatting import FormatPrinter
 from player import Player
+import numpy as np
 
 class Auction:
     def __init__(self, players):
@@ -10,45 +11,43 @@ class Auction:
     def conduct_round(self):
         round_infos = {player.player_id: player.generate_round_info() for player in self.players}
         # pairs of (val, submit_by)
-
-        player_ids = [player.player_id for player in self.players] 
+        
         strategies = {player.player_id: player.determine_strategy() for player in self.players}
 
         cutoff_time = random.uniform(0.7, 1.0) # when this simulator actually stops taking bids
         
         # determining the winner and price logic
 
-        bid_order = sorted([(i, strategies[i][1]) for i in range(len(self.players))
-                            if strategies[i][1] < cutoff_time],
+        bid_order = sorted([(i, strategies[i][2]) for i in range(len(self.players))
+                            if strategies[i][2] < cutoff_time],
                            key = lambda x: x[1])
         # (bidder, time) for time that's eligible.
-        # if there is a tie on timing, we randomize it
-        if len(bid_order) == 2 and bid_order[0][1] == bid_order[1][1]:
-            bid_order = random.shuffle(bid_order)
-        
+
         winning_bid = None
         winning_value = None
+
         if len(bid_order) == 0:
             winner = None
             winning_bid = None
         else:
             if len(bid_order) == 1:
                 winner = bid_order[0][0]
-                winning_bid = strategies[winner][1]
+                winning_bid = strategies[winner][1] 
             else:
                 assert len(bid_order) == 2
-                
-                # player 2 wins if they find it worthwhile underbidding player 1
                 first_player = bid_order[0][0]
                 second_player = bid_order[1][0]
-                second_player_val = self.players[second_player].val
-                winning_bid = strategies[first_player][1]
-                # this will always win 
-                if second_player_val > winning_bid:
+                first_bid_amount = self.players[first_player].val * strategies[first_player][1]
+                second_bid_amount = self.players[second_player].val * strategies[second_player][1]
+                if second_bid_amount > first_bid_amount:
                     winner = second_player
                 else:
                     winner = first_player
+
+            winning_bid = self.players[winner].val * strategies[winner][1]
             winning_value = self.players[winner].val - winning_bid
+
+
         
         # Collect round data
         round_data = {
@@ -56,22 +55,20 @@ class Auction:
             "strategies": strategies,
             "cutoff_time": cutoff_time,
             "bid_order": bid_order,
-            "winner": (winner, winning_bid, winning_value)
+            "winner": (winner, winning_bid, winning_value) if winner is not None else None
         }
         return round_data
 
     def run_simulation(self, num_rounds):
         round_results = []
         winnings = [0.0, 0.0]
-        wincount = [0,0]
         for _ in range(num_rounds):
             round_data = self.conduct_round()
-            if round_data["winner"][0] is not None:
+            if round_data["winner"] is not None and round_data["winner"][0] is not None:
                 w = round_data["winner"][0]
                 winnings[w] += round_data["winner"][2]
-                wincount[w] += 1
             round_results.append(round_data)
-        return (round_results, winnings, wincount)
+        return (round_results, winnings)
 
     def get_results(self):
         return self.round_results
@@ -81,13 +78,17 @@ def test(num_rounds):
     speeds = [1.0, 1.0] # same for now...
     players = [Player(player_id=i, speed=speeds[i]) for i in range(2)]
     auction = Auction(players)
-    round_results, winnings, wincount = auction.run_simulation(num_rounds)
+    round_results, winnings = auction.run_simulation(num_rounds)
 
     for i, result in enumerate(round_results, start=1):
         print(f"Round {i}")
-        FormatPrinter({float: "%.2f"}).pprint(result)
+        FormatPrinter({float: "%.4f"}).pprint(result)
         # from [python - pprint with custom float formats - Stack Overflow](https://stackoverflow.com/questions/44356693/pprint-with-custom-float-formats)
         print("\n")
     
     print(f"Winnings: {winnings}")
-    print(f"Win counts: {wincount}")
+
+import main; main.test(1000)
+
+
+
