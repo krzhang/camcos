174345192094d6c1813bbc204c2b812507c8a80b
Hyojoon Jung <hyojoon119@gmail.com>
2025-04-28 00:10:42 -0700

    final auction simulation

--- simulations/builder_auctions/simulation_1.py
+++ simulations/builder_auctions/simulation_1.py
@@ -2,15 +2,22 @@ import player
 from main import Auction
 from formatting import FormatPrinter
 
-p1 = player.GaussianRangePlayer(0, 0.9, (0.75, 0.33))
-p2 = player.GaussianRangePlayer(1, 0.5, (0.88, 0.42))
+p1 = player.GaussianRangePlayer(0, # p1: slow bidder
+                                (0.0, 0.7), # p1 speed: range of submit time including latency factor in mind(bids early, high latency)
+                                (0.75, 0.33), # p1 mean and variance for bid proportion distribution
+                                )
+p2 = player.ReactiveGaussianRangePlayer(1, # p2: fast bidder
+                                        (0.5, 0.8), # p2 speed: range of submit time(bids later, low latency)
+                                        (0.88, 0.42), # p2 mean and variance for bid proportion distribution
+                                        (0.75, 0.33) # p1's bid proportion distribution known to p2
+                                        )
 
 auction = Auction([p1, p2])
-round_results, winnings, wincount = auction.run_simulation(50)
+round_results, winnings = auction.run_simulation(50)
 
 for i, result in enumerate(round_results, start=1):
     print(f"Round {i}")
-    FormatPrinter({float: "%.2f"}).pprint(result)
+    FormatPrinter({float: "%.3f"}).pprint(result)
     # from [python - pprint with custom float formats - Stack Overflow](https://stackoverflow.com/questions/44356693/pprint-with-custom-float-formats)
     print("\n")
     
